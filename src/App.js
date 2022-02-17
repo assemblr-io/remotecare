b19dc87ba1ded33b0b6f80fd748071abfae31e93
chando <dave@staticum.net>
2022-02-17 12:56:25 +0800

    finish basic layout and styles are in place in App.js and global css setup in index.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="App-nav">NAV</nav>
+      <header className="App-header">HEADER</header>
+      <div className="App-wrapper">
+        <section className="Map">MAP</section>
+        <aside className="App-details">DETAILS</aside>
+      </div>
+      <footer className="App-footer">FOOTER</footer>
     </div>
   );
 }
